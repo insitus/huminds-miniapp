9e0b64dd77b2f4341d345b1449e3d9ee7b814459
Damian <damian.buzzaqui@gmail.com>
2024-04-28 00:58:08 +0300

    audio player example

--- src/components/ContentPage.tsx
+++ src/components/ContentPage.tsx
@@ -1,14 +1,29 @@
 import { Button } from "@nextui-org/react";
+import { AudioPlayer } from "./AudioPlayer/AudioPlayer";
 
 interface Props {
     onBackPress: () => void;
 }
 export function ContentPage({ onBackPress }: Props) {
     return (
-        <div>
-            <h1>Content Page</h1>
+        <div className="mt-4">
+            <div className="flex justify-center mb-4">
+                <h1 className="text-2xl font-medium">Dobro pozhalobat</h1>
+            </div>
 
-            <div className="m-40">
+            <p>
+                Добро пожаловать в Huminds! Этот практический урок создан специально для того, чтобы помочь вам разобраться в том, как устроены ваши эмоции, и начать освобождаться от энергии страха и тревоги.
+
+                Пройдя всю теорию и сделав аудио-практику, вы познакомитесь с мощным инструментом для отпускания негативных эмоций. Уже совсем скоро вы почувствуете прилив жизненной энергии, больше расслабленности, ясности ума и уверенности в себе.
+                Приготовьте бумагу и ручку. Делайте паузы по мере изучения материала, записывайте и применяйте новые знания к своей ситуации.
+                Переходите к следующему аудио. Мы начинаем!
+            </p>
+
+            <div className="flex justify-center  mt-6">
+                <AudioPlayer />
+            </div>
+
+            <div className="mt-20">
                 <Button onClick={onBackPress}>
                     Go back
                 </Button>
--- src/components/ModuleCard.tsx
+++ src/components/ModuleCard.tsx
@@ -17,21 +17,17 @@ export function ModuleCard({ thumbnail, title, underTitle, upperTitle, onSelect 
             onClick={onSelect}
         >
             <CardBody>
-                <div className="grid grid-cols-4 items-center p-2">
+                <div className="grid grid-cols-4 items-center p-1">
 
-                    <div className="flex flex-col col-span-3">
-                        <div className="flex justify-between items-start">
-                            <div className="flex flex-col gap-0">
-                                <h2 className="text-medium font-medium">{upperTitle}</h2>
-                                <h1 className="text-large font-semibold mt-2">{title}</h1>
-                                <h3 className="text-small text-foreground/80 mt-1">{underTitle}</h3>
-                            </div>
-                        </div>
+                    <div className="flex flex-col col-span-3 items-start justify-start">
+                        <h2 className="text-medium font-medium">{upperTitle}</h2>
+                        <h1 className="text-large font-semibold mt-2">{title}</h1>
+                        <h3 className="text-small text-foreground/80 mt-1">{underTitle}</h3>
                     </div>
 
-                    <div className="flex items-center col-span-1">
+                    <div className="flex items-center justify-end">
                         <Image
-                            alt=""
+                            alt="module thumbnail"
                             height={200}
                             width={200}
                             shadow="md"
